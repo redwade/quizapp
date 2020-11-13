74848b25e738d803bed57d1662659e87b0c908cc
Tomin <tominjose222@gmail.com>
2020-11-13 22:27:05 +0530

    Quiz app Deploying stage

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const routes = require('./routes') // includes the routes.js file
+const cors = require('cors')
+
+
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('database connected'))
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log("The API is running...")
+})
